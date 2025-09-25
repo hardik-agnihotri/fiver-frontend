36bcc9f2fe7f0f58f3dd612d52f2e8456029d2fd
hardik <hardik22oct@gmail.com>
2025-09-26 03:20:40 +0530

    navbar completed & rrd added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Footer from "./components/footer/Footer";
+import MyGigs from "./pages/myGigs/MyGigs";
+import Add from "./pages/add/Add";
+import Orders from "./pages/orders/Orders"
+import Messages from "./pages/messages/Messages"
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/my-gigs" element={<MyGigs />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./Navbar.scss";
+import img from "../../assets/user.png";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const [profileDrop, setprofileDrop] = useState(false);
   const [active, setActive] = useState(false);
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
+  const { pathname } = useLocation();
 
   useEffect(() => {
     window.addEventListener("scroll", isActive);
@@ -15,35 +19,96 @@ const Navbar = () => {
     };
   }, []);
 
+  const currentUser = {
+    id: 1,
+    username: "Hardik",
+    isSeller: true,
+  };
+
   return (
-    <div className={active ? "navbar active " : "navbar"}>
+    <div className={active || pathname !== "/" ? "navbar active " : "navbar"}>
       <div className="container">
         <div className="logo">
-          <span className="logo-txt">ziGGLer</span>
+          <Link to="/" className="logo-txt link">
+            ziGGLer
+          </Link>
           <span className="logo-dot">.</span>
         </div>
         <div className="links">
           <span>ziGGLer Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Sign In</span>
-          <span>Become a sellar</span>
-          <button className={active ? "btn-chg":""}>Join</button>
+          {!currentUser && <span>Sign In</span>}
+          {!currentUser?.isSeller && <span>Become a sellar</span>}
+          {!currentUser && (
+            <button className={active ? "btn-chg" : ""}>Join</button>
+          )}
+          {currentUser && (
+            <div
+              className="user"
+              onClick={() => setprofileDrop((prev) => !prev)}
+            >
+              <img src={img} alt="" />
+              <span>{currentUser.username}</span>
+              {profileDrop && (
+                <div className="options">
+                  {currentUser.isSeller && (
+                    <>
+                      <span>
+                        <Link to="/my-gigs" className="link">
+                          Gigs
+                        </Link>
+                      </span>
+                      <span>
+                        <Link to="/add" className="link">
+                          Add new Gig
+                        </Link>
+                      </span>
+                    </>
+                  )}
+                  <span>
+                    <Link to="/orders" className="link">
+                      Orders
+                    </Link>
+                  </span>
+                  <span>
+                    <Link to="/messages" className="link">
+                      Messages
+                    </Link>
+                  </span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
-      {active && (
-        <>
-          <hr />
-          <div className="menu">
-            <span>Text</span>
-            <span>Text</span>
-            <span>Text</span>
-            <span>Text</span>
-            <span>Text</span>
-            <span>Text</span>
-          </div>
-        </>
-      )}
+      {active ||
+        (pathname !== "/" && (
+          <>
+            <hr />
+            <div className="menu">
+              <Link to="/" className="link">
+                Web Development
+              </Link>
+              <Link to="/" className="link">
+                Graphic Design
+              </Link>
+              <Link to="/" className="link">
+                Digital Marketing
+              </Link>
+              <Link to="/" className="link">
+                Writing & Translation
+              </Link>
+              <Link to="/" className="link">
+                Video Editing
+              </Link>
+              <Link to="/" className="link">
+                Data Entry
+              </Link>
+            </div>
+          </>
+        ))}
     </div>
   );
 };
