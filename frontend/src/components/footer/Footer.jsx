1265872138c955651bd2d7d5d942439459d9cfb0
hardik <hardik22oct@gmail.com>
2025-09-30 03:09:02 +0530

    home page done

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import "./footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="footer">
+      <div className="container">
+        <div className="f-cat">
+          <div className="f-list">
+            <h2>Categories</h2>
+            <span className="f-link">Graphic design</span>
+            <span className="f-link">Web Development</span>
+            <span className="f-link">React Development</span>
+            <span className="f-link">Logo Design</span>
+            <span className="f-link">Figma Design</span>
+            <span className="f-link">Android Development</span>
+            <span className="f-link">Website Design</span>
+          </div>
+          <div className="f-list">
+            <h2>Categories</h2>
+            <span className="f-link">Graphic design</span>
+            <span className="f-link">Web Development</span>
+            <span className="f-link">React Development</span>
+            <span className="f-link">Logo Design</span>
+            <span className="f-link">Figma Design</span>
+            <span className="f-link">Android Development</span>
+            <span className="f-link">Website Design</span>
+          </div>
+          <div className="f-list">
+            <h2>Categories</h2>
+            <span className="f-link">Graphic design</span>
+            <span className="f-link">Web Development</span>
+            <span className="f-link">React Development</span>
+            <span className="f-link">Logo Design</span>
+            <span className="f-link">Figma Design</span>
+            <span className="f-link">Android Development</span>
+            <span className="f-link">Website Design</span>
+          </div>
+          <div className="f-list">
+            <h2>Categories</h2>
+            <span className="f-link">Graphic design</span>
+            <span className="f-link">Web Development</span>
+            <span className="f-link">React Development</span>
+            <span className="f-link">Logo Design</span>
+            <span className="f-link">Figma Design</span>
+            <span className="f-link">Android Development</span>
+            <span className="f-link">Website Design</span>
+          </div>
+        </div>
+        <div className="f-cat-btm">
+          <div className="logo">ziGGler</div>
+          <div className="f-btm-link">
+            <div className="social-link">
+              <img src="./img/instagram.png" alt="" />
+              <img src="./img/facebook.png" alt="" />
+              <img src="./img/linkedin.png" alt="" />
+              <img src="./img/twitter.png" alt="" />
+            </div>
+            <div className="url-link">
+              <img src="./img/language.png" alt="" />
+              <img src="./img/accessibility.png" alt="" />
+              <img src="./img/coin.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
